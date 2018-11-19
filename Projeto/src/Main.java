6fbb22aed99722f9a8165076b2c9ff14cbae8fd0
Camila Faria <camila_faria.castro@hotmail.com>
2018-11-19 16:07:26 -0200

    Alterações no código

--- Projeto/src/Main.java
+++ Projeto/src/Main.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,61 +22,56 @@ public class Main {
 		insertDatabase(tweets);
 	}
 
-	// lê arquivo CSV e cria dataset de tweets
-	public static ArrayList<Tweet> readArchive() throws IOException {
-		Scanner scanner = new Scanner(new File("datasetTweet.csv"), "UTF-8");
-		Scanner dataScanner = null;
-		int index = 0;
+	public static ArrayList<Tweet> readArchive() throws IOException {		
+		String arquivoCSV = "datasetTweet.csv";
+		BufferedReader br = null;
+		String linha = "";
+		String csvDivisor = ";";
 		ArrayList<Tweet> tweetDataset = new ArrayList<>();
-
-		while (scanner.hasNextLine()) {
-			dataScanner = new Scanner(scanner.nextLine());
-			dataScanner.useDelimiter(";");
-			Tweet tweet = new Tweet();
-
-			while (dataScanner.hasNext()) {
-				String data = dataScanner.next();
-				if (index == 0)
-					tweet.setId(Long.parseLong(data));
-				else if (index == 1)
-					tweet.setDate(data);
-				else if (index == 2)
-					tweet.setTime(data);
-				else if (index == 3)
-					tweet.setText(data);
-				else if (index == 4)
-					tweet.setHashtag(data);
-				else if (index == 5)
-					tweet.setSerie(data);
-				else if (index == 6)
-					tweet.setLanguage(data);
-				else
-					System.out.println("invalid data::" + data);
-
-				index++;
+		try {			
+			br = new BufferedReader(new FileReader(arquivoCSV));
+			while ((linha = br.readLine()) != null) {
+				Tweet tweet = new Tweet();
+				String[] twt = linha.split(csvDivisor);
+				tweet.setId(Long.parseLong(twt[0]));
+				tweet.setDate(twt[1]);
+				tweet.setTime(twt[2]);
+				tweet.setText(twt[3]);
+				tweet.setHashtag(twt[4]);
+				tweet.setSerie(twt[5]);
+				tweet.setLanguage(twt[6]);
+				tweetDataset.add(tweet);
+			}			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-
-			index = 0;
-			tweetDataset.add(tweet);
 		}
-
-		scanner.close();
 		return tweetDataset;
-	}
+	}	
 
-	// faz um pré-processamento nos tweets, traduz para a língua inglesa e calcula o sentimento
+	// faz um pré-processamento nos tweets, traduz para a língua inglesa e calcula o
+	// sentimento
 	public static ArrayList<Tweet> processing(ArrayList<Tweet> tweets) {
-		
+
 		SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
 		sentimentAnalyzer.initialize();
-		
+
 		for (Tweet tweet : tweets) {
 			String text = Translation.translate(PreProcessing.preprocessar(tweet.getText()), tweet.getLanguage());
 			System.out.println(text);
 			tweet.setProcessedText(text);
-			tweet.setSentiment(sentimentAnalyzer.getSentimentResult(text));						
+			tweet.setSentiment(sentimentAnalyzer.getSentimentResult(text));
 		}
-		
+
 		return tweets;
 	}
 
--- Projeto/src/com/rmtheis/yandtran/ApiKeys.java
+++ Projeto/src/com/rmtheis/yandtran/ApiKeys.java
@@ -1,5 +1,5 @@
 package com.rmtheis.yandtran;
 
 public class ApiKeys {
-  public static final String YANDEX_API_KEY = "trnsl.1.1.20181118T155812Z.44352d844038a8b0.d8a464d3173ca984971379f8bdfcaab329a634bb";
+  public static final String YANDEX_API_KEY = "trnsl.1.1.20181119T150031Z.119dbef520abcc4d.50f34df48887c221340ee7927b45762ee4ed642d";
 }
